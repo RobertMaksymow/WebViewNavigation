fb5c89408c62a2b93971735645c3aa4ab8a5237b
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2024-02-22 20:21:36 +0000

    more cleaning

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,18 +15,6 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
 function App(): React.JSX.Element {
   return (
