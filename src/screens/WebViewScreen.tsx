99ce41861c5b4b06ab3a7e1a8699855735deea23
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2024-02-23 11:05:32 +0000

    back button added to the webview screen

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -1,16 +1,25 @@
-import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, ActivityIndicator, Button} from 'react-native';
 import React, {useRef} from 'react';
 import {WebView} from 'react-native-webview';
 
 const WebViewScreen = ({navigation}) => {
+  const webViewRef = useRef<WebView>(null);
+
   return (
     <>
-      {/* <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 0, alignItems: 'center', justifyContent: 'center'}}>
         <Text>WebView Screen</Text>
-      </View> */}
+        <Button title="Go Back" onPress={() => webViewRef.current?.goBack()} />
+        <Button title="Reload" onPress={() => webViewRef.current?.reload()} />
+        <Button
+          title="Go Forward"
+          onPress={() => webViewRef.current?.goForward()}
+        />
+      </View>
       <WebView
         source={{uri: 'https://www.google.com/search?q=react+native+school'}}
         startInLoadingState
+        ref={webViewRef}
         renderLoading={() => (
           <View style={{flex: 1, alignItems: 'center'}}>
             <ActivityIndicator size="large" />
